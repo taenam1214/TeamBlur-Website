621fd0ead4d5cd3a432d3e82d46da214542e2717
taenam <tkim356@wisc.edu>
2025-03-07 16:24:50 -0600

    layout added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Plus_Jakarta_Sans, DynaPuff } from "next/font/google";
 import "@/app/globals.css";
+import { Analytics } from "@vercel/analytics/react"
 
 const plusJakartaSans = Plus_Jakarta_Sans({
   subsets: ["latin"],
@@ -22,7 +23,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={`${plusJakartaSans.variable} ${dynaPuff.variable}`}>
-      <body>{children}</body>
+      <body>{children} <Analytics /> </body>
     </html>
   );
 }
